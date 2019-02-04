bd284953522781170020ae76921d5916422589fa
rapin <alexis.rapin@chuv.ch>
2019-02-04 17:31:47 +0100

    tweak config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
+environment_variables = File.join(Rails.root, 'config', 'environment_variables.rb')
+load(environment_variables) if File.exists?(environment_variables)
+
 # Initialize the Rails application.
 Rails.application.initialize!
